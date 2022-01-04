6593e213b1e2f52da7b76734c477733b2e9742cb
liebeSonne <>
2022-01-04 15:20:01 +0300

    lab2 - Добавил в интерфейс функции переменную приоритета

--- labs/02/src/Observable/ObservableInterface.php
+++ labs/02/src/Observable/ObservableInterface.php
@@ -6,7 +6,7 @@ use App\Observer\ObserverInterface;
 
 interface ObservableInterface
 {
-    public function registerObserver(ObserverInterface $observer) : void;
+    public function registerObserver(ObserverInterface $observer, int $priority = 0) : void;
     public function notifyObservers() : void;
     public function removeObserver(ObserverInterface $observer) : void;
 }
