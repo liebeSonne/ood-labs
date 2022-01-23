12f9dd9d28269574dc962ce5492daf4d597aef03
liebeSonne <>
2022-01-23 22:49:01 +0300

    lab 2 - добавил тест для сигналов

--- labs/02/tests/Unit/Model/Weather/WeatherDataSignalTest.php
+++ labs/02/tests/Unit/Model/Weather/WeatherDataSignalTest.php
@@ -1,22 +1,26 @@
 <?php
 
-//namespace Tests\Unit\Model\Weather;
-//
-//use App\Model\Weather\WeatherDataSignal;
-//use PHPUnit\Framework\TestCase;
-//
-//class WeatherDataSignalTest extends TestCase
-//{
-//    public function testSetMeasurements()
-//    {
-//        $weather = $this->createMock(WeatherDataSignal::class);
-//
-//        $weather->expects($this->exactly(3))->method('emit')->withConsecutive(
-//            [$this->equalTo('temp'), $this->greaterThan(0)],
-//            [$this->equalTo('humidity'), $this->greaterThan(0)],
-//            [$this->equalTo('pressure'), $this->greaterThan(0)],
-//        );
-//
-//        $weather->setMeasurements(5, 10, 20);
-//    }
-//}
+namespace Tests\Unit\Model\Weather;
+
+use App\Model\Weather\WeatherDataSignal;
+use PHPUnit\Framework\TestCase;
+
+class WeatherDataSignalTest extends TestCase
+{
+    public function testSetMeasurements()
+    {
+        $weather = $this->getMockBuilder(WeatherDataSignal::class)
+            ->setMethods(['emit','measurementsChanged'])
+            ->getMock();
+
+        $weather->expects($this->once())->method('measurementsChanged');
+
+        $weather->expects($this->exactly(3))->method('emit')->withConsecutive(
+            [$this->equalTo('temp'), $this->greaterThan(0)],
+            [$this->equalTo('humidity'), $this->greaterThan(0)],
+            [$this->equalTo('pressure'), $this->greaterThan(0)],
+        );
+
+        $weather->setMeasurements(5, 10, 20);
+    }
+}
