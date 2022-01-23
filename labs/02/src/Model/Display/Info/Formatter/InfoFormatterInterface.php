702df8a9921da2a1961697a4bd22fac21feac2b4
liebeSonne <>
2022-01-23 23:38:38 +0300

    lab 2 - исправил аргумент в интерфейе

--- labs/02/src/Model/Display/Info/Formatter/InfoFormatterInterface.php
+++ labs/02/src/Model/Display/Info/Formatter/InfoFormatterInterface.php
@@ -2,9 +2,7 @@
 
 namespace App\Model\Display\Info\Formatter;
 
-use App\Model\Weather\WeatherInfo;
-
 interface InfoFormatterInterface
 {
-    public function display(WeatherInfo $data): void;
+    public function display(\StdClass $data): void;
 }
