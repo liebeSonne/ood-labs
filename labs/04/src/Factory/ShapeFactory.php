95f88f84358abbd2c28c59fa954f221de04883a9
liebeSonne <>
2022-03-10 23:11:41 +0300

    lab4 - разделил на методы парсинг фигур

--- labs/04/src/Factory/ShapeFactory.php
+++ labs/04/src/Factory/ShapeFactory.php
@@ -3,6 +3,7 @@
 namespace App\Factory;
 
 use App\Color;
+use App\Command\Editor\EditorListCommand;
 use App\Point;
 use App\Shape\Ellipse;
 use App\Shape\Rectangle;
@@ -23,38 +24,58 @@ class ShapeFactory implements ShapeFactoryInterface
 
         switch ($name) {
             case 'rectangle':
-                $color = Color::BLACK;
-                $count = sscanf($description,'%s%f%f%f%f%s', $name, $x1, $y1, $x2, $y2, $color);
-                if ($count < 5) return null;
-                $leftTop = new Point($x1, $y1);
-                $rightBottom = new Point($x2, $y2);
-                return new Rectangle($leftTop, $rightBottom, $color);
+                return $this->createRectangle($description);
             case 'triangle':
-                $color = Color::BLACK;
-                $count = sscanf($description,'%s%f%f%f%f%f%f%s', $name, $x1, $y1, $x2, $y2, $x3, $y3, $color);
-                if ($count < 7) return null;
-                $p1 = new Point($x1, $y1);
-                $p2 = new Point($x2, $y2);
-                $p3 = new Point($x3, $y3);
-                return new Triangle($p1, $p2, $p3, $color);
+                return $this->createTriangle($description);
             case 'ellipse':
-                $color = Color::BLACK;
-                $count = sscanf($description,'%s%f%f%f%f%s', $name, $x1, $y1, $hr, $vr, $color);
-                if ($count < 5) return null;
-                $center = new Point($x1, $y1);
-                $hRadius = $hr;
-                $vRadius = $vr;
-                return new Ellipse($center, $hRadius, $vRadius, $color);
+                return $this->createEllipse($description);
             case 'polygon':
-                $color = Color::BLACK;
-                $count = sscanf($description,'%s%f%f%f%d%s', $name, $x1, $y1, $r, $cv, $color);
-                if ($count < 5) return null;
-                $center = new Point($x1, $y1);
-                $radius = $r;
-                $countVertex = $cv;
-                return new RegularPolygon($center, $radius, $countVertex, $color);
+                return $this->createRegularPolygon($description);
             default:
                 return null;
         }
+    }
+
+    private function createRectangle(string $description): ?Rectangle
+    {
+        $color = Color::BLACK;
+        $count = sscanf($description,'%s%f%f%f%f%s', $name, $x1, $y1, $x2, $y2, $color);
+        if ($count < 5) return null;
+        $leftTop = new Point($x1, $y1);
+        $rightBottom = new Point($x2, $y2);
+        return new Rectangle($leftTop, $rightBottom, $color);
+    }
+
+    private function createTriangle(string $description): ?Triangle
+    {
+        $color = Color::BLACK;
+        $count = sscanf($description,'%s%f%f%f%f%f%f%s', $name, $x1, $y1, $x2, $y2, $x3, $y3, $color);
+        if ($count < 7) return null;
+        $p1 = new Point($x1, $y1);
+        $p2 = new Point($x2, $y2);
+        $p3 = new Point($x3, $y3);
+        return new Triangle($p1, $p2, $p3, $color);
+    }
+
+    private function createEllipse(string $description): ?Ellipse
+    {
+        $color = Color::BLACK;
+        $count = sscanf($description,'%s%f%f%f%f%s', $name, $x1, $y1, $hr, $vr, $color);
+        if ($count < 5) return null;
+        $center = new Point($x1, $y1);
+        $hRadius = $hr;
+        $vRadius = $vr;
+        return new Ellipse($center, $hRadius, $vRadius, $color);
+    }
+
+    private function createRegularPolygon(string $description): ?RegularPolygon
+    {
+        $color = Color::BLACK;
+        $count = sscanf($description,'%s%f%f%f%d%s', $name, $x1, $y1, $r, $cv, $color);
+        if ($count < 5) return null;
+        $center = new Point($x1, $y1);
+        $radius = $r;
+        $countVertex = $cv;
+        return new RegularPolygon($center, $radius, $countVertex, $color);
     }
 }
