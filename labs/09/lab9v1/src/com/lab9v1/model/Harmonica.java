89824a9d9bcf0616ba55b766dc9fc31c2112f5db
liebeSonne <>
2022-04-20 00:30:05 +0300

    lab9 - добавил класс гармоничных формул

---
+++ labs/09/lab9v1/src/com/lab9v1/model/Harmonica.java
@@ -0,0 +1,52 @@
+package com.lab9v1.model;
+
+public class Harmonica {
+    private double amplitude;
+    private Formula formula;
+    private int frequency;
+    private int phase;
+
+    public Harmonica(double amplitude, Formula formula, int frequency, int phase) {
+        setAmplitude(amplitude);
+        setFormula(formula);
+        setFrequency(frequency);
+        setPhase(phase);
+    }
+
+    public double getAmplitude() {
+        return amplitude;
+    }
+
+    public Formula getFormula() {
+        return formula;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public int getPhase() {
+        return phase;
+    }
+
+    public void setAmplitude(double amplitude) {
+        this.amplitude = amplitude;
+    }
+
+    public void setFormula(Formula formula) {
+        this.formula = formula;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public void setPhase(int phase) {
+        this.phase = phase;
+    }
+
+    @Override
+    public String toString() {
+        return getAmplitude() + "*" + getFormula() + "(" + getFrequency() + "*x+" + getPhase() + ")";
+    }
+}
