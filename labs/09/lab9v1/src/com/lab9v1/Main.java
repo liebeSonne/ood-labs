68e7d037dd43e01cb5fae6bb8b3d2a30f7ae460f
liebeSonne <>
2022-05-02 22:39:27 +0300

    lab9 - добавил инициализацию начального списка формул гармоник

--- labs/09/lab9v1/src/com/lab9v1/Main.java
+++ labs/09/lab9v1/src/com/lab9v1/Main.java
@@ -1,16 +1,20 @@
 package com.lab9v1;
 
 import com.lab9v1.controller.MainController;
-import com.lab9v1.model.Harmonica;
+import com.lab9v1.model.Document;
+import com.lab9v1.model.Formula;
 import com.lab9v1.view.App;
 
 import javax.swing.*;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Harmonica> harmonics = new ArrayList<Harmonica>();
-        MainController controller = new MainController(harmonics);
+        Document document = new Document();
+        MainController controller = new MainController(document);
+
+        document.addHarmonica(3, Formula.SIN, -3, 0.3);
+        document.addHarmonica(4.38, Formula.SIN, 2.25, 1.5);
+        document.addHarmonica(1, Formula.COS, 1, 5);
 
         App app = new App(controller);
         JPanel jPanel = app.getMainPanel();
