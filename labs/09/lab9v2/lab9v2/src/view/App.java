03aef29b1bb034dfb2fbdf1faa313a1230c7bae5
liebeSonne <>
2022-05-10 21:50:37 +0300

    lab9v2 - изменил размеры области редактора

--- labs/09/lab9v2/lab9v2/src/view/App.java
+++ labs/09/lab9v2/lab9v2/src/view/App.java
@@ -20,7 +20,7 @@ public class App extends JFrame {
         this.setContentPane(this.mainPanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
-        this.setSize(800, 600);
+        this.setSize(800, 700);
         this.setVisible(true);
     }
 
