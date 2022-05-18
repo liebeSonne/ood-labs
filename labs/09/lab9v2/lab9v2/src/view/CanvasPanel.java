70c18fae686140ff444c992cb7acdb010fee68a4
liebeSonne <>
2022-05-19 00:45:53 +0300

    lab9v2 - добавил слушание событий мыши, выделение фигры и рисование рамки выделенной фигруы

--- labs/09/lab9v2/lab9v2/src/view/CanvasPanel.java
+++ labs/09/lab9v2/lab9v2/src/view/CanvasPanel.java
@@ -2,15 +2,22 @@ package view;
 
 import canvas.GraphicsCanvas;
 import document.DocumentInterface;
+import shape.ShapeInterface;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 public class CanvasPanel extends JPanel {
     private DocumentInterface document;
 
-    public CanvasPanel() {
+    private ShapeInterface selectedShape;
 
+    public CanvasPanel() {
+        super();
+        this.bindMouseListener();
     }
 
     public void setDocument(DocumentInterface document) {
@@ -27,6 +34,58 @@ public class CanvasPanel extends JPanel {
             this.document.getShapes().forEach(shape -> {
                 shape.draw(canvas);
             });
+            if (this.selectedShape != null) {
+                this.selectedShape.drawFrame(canvas);
+            }
         }
+    }
+
+    private void redraw() {
+        this.paintComponents(this.getGraphics());
+    }
+
+    private void selectShape(Point point) {
+        this.selectedShape = null;
+        this.document.getShapes().forEach(shape -> {
+            if (shape.contains(point)) {
+                selectedShape = shape;
+            }
+        });
+        System.out.println("selectShape: " + this.selectedShape);
+        this.redraw();
+    }
+
+    private void bindMouseListener() {
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+                System.out.println("mousePressed: " + e.getPoint());
+                selectShape(e.getPoint());
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+                System.out.println("mouseReleased: " + e.getPoint());
+                setCursor(Cursor.getPredefinedCursor(Cursor.getDefaultCursor().getType()));
+            }
+        });
+
+        this.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+            super.mouseDragged(e);
+            System.out.println("mouseDragged: " + e.getPoint());
+            setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+            }
+//
+//            @Override
+//            public void mouseMoved(MouseEvent e) {
+//                super.mouseMoved(e);
+//                System.out.println("mouseMoved: " + e.getPoint());
+//            }
+
+        });
     }
 }
