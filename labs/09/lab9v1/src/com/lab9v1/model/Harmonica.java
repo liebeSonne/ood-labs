a467158857fe5e46c01d74f7206b6fc3dcb14b4e
liebeSonne <>
2022-05-19 21:57:26 +0300

    lab9v1 - удалил лишний setter интерфейс

--- labs/09/lab9v1/src/com/lab9v1/model/Harmonica.java
+++ labs/09/lab9v1/src/com/lab9v1/model/Harmonica.java
@@ -1,6 +1,6 @@
 package com.lab9v1.model;
 
-public class Harmonica implements HarmonicaExecutor, HarmonicaSetter, ImmutableHarmonica {
+public class Harmonica implements HarmonicaExecutor, ImmutableHarmonica {
     private double amplitude;
     private Formula formula;
     private double frequency;
