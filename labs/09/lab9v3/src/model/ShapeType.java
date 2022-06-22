83caa18ced2f67ebbd512ec401884b7865ae9e24
liebeSonne <>
2022-06-23 01:10:36 +0300

    lab9v3 - добавил типы фигур

---
+++ labs/09/lab9v3/src/model/ShapeType.java
@@ -0,0 +1,22 @@
+package model;
+
+public enum ShapeType {
+    ELLIPSE {
+        @Override
+        public String toString () {
+            return "Ellipse";
+        }
+    },
+    TRIANGLE {
+        @Override
+        public String toString () {
+            return "Triangle";
+        }
+    },
+    RECTANGLE {
+        @Override
+        public String toString () {
+            return "Rectangle";
+        }
+    }
+}
