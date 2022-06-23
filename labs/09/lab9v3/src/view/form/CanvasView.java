fabec5a61e230e4f9bebc6c6becb426816a25c09
liebeSonne <>
2022-06-23 22:20:39 +0300

    lab9v3 - добавил рисование фигур на canvas

--- labs/09/lab9v3/src/view/form/CanvasView.java
+++ labs/09/lab9v3/src/view/form/CanvasView.java
@@ -3,6 +3,8 @@ package view.form;
 import controller.CanvasController;
 import model.Document;
 import view.data.ShapeDataViewInterface;
+import view.shape.ShapeViewFactory;
+import view.shape.ShapeViewInterface;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,14 +12,30 @@ import java.awt.*;
 public class CanvasView extends JPanel implements ShapeDataViewInterface {
     private Document document;
     private CanvasController controller;
+
+    private ShapeViewFactory factory;
     public CanvasView(Document document) {
         super();
         this.document = document;
         this.controller = new CanvasController(document);
+        this.factory = new ShapeViewFactory();
     }
 
     @Override
     public Point getCenter() {
         return new Point(this.getWidth() /2, this.getHeight() / 2);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        document.forEach(shape -> {
+            ShapeViewInterface view = factory.createShapeView(shape);
+            if (view != null) {
+                view.draw(g2);
+            }
+        });
     }
 }
